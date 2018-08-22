797311b1aa2e4c0c8f7610da656ee88d366cd3f3
Kyle McGuffin <kyle.mcguffin@vanderbilt.edu>
2018-08-21 20:50:22 -0400

    Basic template to start the external modules templater

---
+++ Templater.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mcguffk
+ * Date: 8/21/2018
+ * Time: 7:46 PM
+ */
+
+namespace RedcapConHack\Templater;
+
+class Templater extends \ExternalModules\AbstractExternalModule{
+
+}
